1d88033de2fa4661234a69d8488480161394a785
Anton Huck <antonhuck@gmail.com>
2020-12-03 19:04:38 +0100

    Only run today

--- util.js
+++ util.js
@@ -1,9 +1,4 @@
-const https = require('https')
-var config = require('./config.json');
-const readline = require('readline');
 const fs = require('fs');
-
-const year = 2020
 
 var getInputFromFile = day => {
     if (day < 10) {
@@ -14,60 +9,6 @@ var getInputFromFile = day => {
     return data
 }
 
-var getInput = (day) => {
-
-    const options = {
-        method: "GET",
-        hostname: 'adventofcode.com',
-        port: 443,
-        path: "/" + year + "/day/" + day + "/input",
-        headers: {
-            Cookie: config.cookie
-        }
-    }
-
-    return new Promise((resolve, reject) =>  {
-        const req = https.request(
-            options, 
-            (res) => {
-                res.on('data', d => resolve(d.toString().trim()))
-            }
-        )
-        req.on('error', reject)
-        req.end()
-    })
-}
-
-var sendAnswer = (day, level, answer) => {
-    var data = 'level=' + level + "&answer=" + answer
-
-    const options = {
-        method: "POST",
-        hostname: 'adventofcode.com',
-        port: 443,
-        path: "/" + year + "/day/" + day + "/answer",
-        headers: {
-            Cookie: config.cookie,
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Content-Length": data.length
-        }
-    }
-    return new Promise((resolve, reject) =>  {
-        const req = https.request(
-            options, 
-            res => {
-                res.on('data', d => resolve(d.toString().trim()))
-            }
-        )
-        req.on('error', reject)
-
-        req.write(data)
-        req.end()
-    })
-}
-
 module.exports = {
-    getInput,
-    sendAnswer,
     getInputFromFile
 }
