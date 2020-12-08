e33d4f53fa05bccf71979e6e760bf3707a2cf656
Anton Huck <antonhuck@gmail.com>
2020-12-08 09:31:42 +0100

    Day 8 - js

---
+++ 08.js
@@ -0,0 +1,90 @@
+const util = require('./util.js')
+
+const TODAY = 8
+const REGEXP_BAG = /[0-9] [a-z]+ [a-z]+ bag(s)?/g;
+const INITIAL_BAG = "shiny gold"
+async  function main() {
+    const ops = util.getInputFromFile(TODAY)
+        .split('\n')
+        .map(el => el.split(' '))
+        .map(el => [el[0], parseInt(el[1])])
+    
+    console.log("Part 1: " + part1(ops))
+    console.log("Part 2: " + part2(ops))
+}
+
+
+function part1(ops) {
+    let visited = new Set()
+    let ptr = 0, acc = 0
+
+    while (!visited.has(ptr)) {
+        visited.add(ptr)
+        switch(ops[ptr][0]) {
+            case 'acc':
+                acc += ops[ptr][1]
+                ptr++
+                break
+            case 'jmp':
+                ptr += ops[ptr][1]
+                break
+            default:
+                ptr++
+        }
+    }
+
+    return acc
+}
+
+function part2(ops) {
+    let visited = new Set(), possibleFlaws = new Set()
+    let ptr = 0
+
+    while (!visited.has(ptr)) {
+        visited.add(ptr)
+        switch(ops[ptr][0]) {
+            case 'acc':
+                ptr++
+                break
+            case 'jmp':
+                possibleFlaws.add(ptr)
+                ptr += ops[ptr][1]
+                break
+            default:
+                possibleFlaws.add(ptr)
+                ptr++
+        }
+    }
+
+    for(flaw of Array.from(possibleFlaws)) {
+        let visited = new Set()
+        let opsCopy = JSON.parse(JSON.stringify(ops));
+        let ptr = 0, acc = 0
+
+        opsCopy[flaw][0] = opsCopy[flaw][0] === 'nop' ? 'jmp' : 'nop'
+
+        
+        while (!visited.has(ptr) && ptr<opsCopy.length) {
+            visited.add(ptr)
+            switch(opsCopy[ptr][0]) {
+                case 'acc':
+                    acc += ops[ptr][1]
+                    ptr++
+                    break
+                case 'jmp':
+                    ptr += ops[ptr][1]
+                    break
+                default:
+                    ptr++
+            }
+        }
+
+        if(ptr === opsCopy.length) {
+            return acc
+        }
+    }
+
+    return -1
+}
+
+main();
